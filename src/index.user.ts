0a07754ee0f888dce854984f0cff21389ae6e600
Haoyuan Kevin Xia <kevin062397@outlook.com>
2025-05-02 00:14:29 -0700

    Add custom styles to make checkboxes more prominent when disabled

--- src/index.user.ts
+++ src/index.user.ts
@@ -27,17 +27,69 @@
             let match = text.match(uncheckedRegex);
             if (match) {
                 const label = match[1];
-                paragraphItem.innerHTML = `<input type="checkbox" disabled /> ${label}`;
+                paragraphItem.innerHTML = `<input class="styled-checkbox" type="checkbox" disabled /> ${label}`;
                 return;
             }
 
             match = text.match(checkedRegex);
             if (match) {
                 const label = match[1];
-                paragraphItem.innerHTML = `<input type="checkbox" checked disabled /> ${label}`;
+                paragraphItem.innerHTML = `<input class="styled-checkbox" type="checkbox" checked disabled /> ${label}`;
             }
         });
     };
+
+    // Add custom styles to make checkboxes more prominent
+    const style = document.createElement("style");
+    style.textContent = `
+        input.styled-checkbox {
+            --tint-color: rgba(0, 122, 255, 1);
+            --separator-color: rgba(60, 60, 67, 0.29);
+            /* Color scheme independent properties */
+            --clear-color: transparent;
+            --white-color: rgba(255, 255, 255, 1);
+        }
+
+        @media (prefers-color-scheme: dark) {
+            input.styled-checkbox {
+                --tint-color: rgba(10, 132, 255, 1);
+                --separator-color: rgba(84, 84, 88, 0.6);
+            }
+        }
+
+        input.styled-checkbox {
+            appearance: none;
+            background-color: var(--clear-color);
+            border-radius: 25%;
+            border: 1px solid var(--separator-color);
+            cursor: default;
+            display: inline-block;
+            font-size: 1em;
+            height: 1em;
+            margin: 0;
+            vertical-align: middle;
+            width: 1em;
+        }
+
+        input.styled-checkbox:checked {
+            background-color: var(--tint-color);
+        }
+
+        input.styled-checkbox::before {
+            color: var(--white-color);
+            content: "\\2713"; /* Unicode check mark */
+            display: block;
+            font-size: 0.8em;
+            line-height: calc(1.25em - 2px);
+            text-align: center;
+            visibility: hidden;
+        }
+
+        input.styled-checkbox:checked::before {
+            visibility: visible;
+        }
+    `;
+    document.head.appendChild(style);
 
     // Run on page load
     replaceMarkdownCheckboxes();
